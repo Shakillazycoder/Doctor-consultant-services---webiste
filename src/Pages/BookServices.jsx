30f604e9846b5846e43c1f81c15e5ba62986f2c3
Shakillazycoder <sakil.khan882.sk@gmail.com>
2024-05-13 23:03:13 +0600

    deployment related problems solved

--- src/Pages/BookServices.jsx
+++ src/Pages/BookServices.jsx
@@ -1,33 +1,33 @@
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BookingCard from "../Component/BookingCard";
 import useAuth from "../Hooks/useAuth";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
 
 const BookServices = () => {
-    const {user} = useAuth()
-    const [bookings, SetBookings] = useState([])
-    const axiosSecure = useAxiosSecure()
+  const { user } = useAuth();
+  const [bookings, SetBookings] = useState([]);
+  const axiosSecure = useAxiosSecure();
 
+  const url = `/booking/${user?.email}`;
 
-    const url = `/booking/${user?.email}`;
+  useEffect(() => {
+    axiosSecure.get(url).then((res) => {
+      console.log(res.data);
+      SetBookings(res.data);
+    });
+  }, [url, axiosSecure]);
 
-    useEffect(() => {
-      axiosSecure.get(url)
-         .then((res) => {
-            console.log(res.data);
-            SetBookings(res.data)
-          })
-    }, [url, axiosSecure])
-
-    return (
-        <div>
-            <div className="mt-5 container mx-auto space-y-5 items-center my-20">
-            {bookings.map((booking, index) => (
+  return (
+    <div>
+      <div className="mt-5 container mx-auto space-y-5 items-center my-20">
+        {bookings.length === 0
+          ? <div className="text-3xl font-bold"> No bookings found for your account</div>
+          : bookings.map((booking, index) => (
               <BookingCard key={index} booking={booking} />
             ))}
-          </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookServices;
--- src/Pages/ServicesToDo.jsx
+++ src/Pages/ServicesToDo.jsx
@@ -20,7 +20,7 @@ const ServicesToDo = () => {
     <div className="flex justify-center items-center my-20">
       <div className="mt-5  space-y-5">
         {servicesToDo.length === 0 ? (
-          <div className="text-3xl font-bold"> No bookings found for your account</div>
+          <div className="text-3xl font-bold"> No bookings Services found for your account</div>
         ) : (
           servicesToDo.map((booking, index) => (
             <ServicesToDoCard
